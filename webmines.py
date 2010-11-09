2a2b6988b5f431f217ed5dc7362757c490658abd
Spacerat <spacerat3004@gmail.com>
2010-11-09 19:47:07 +0000

    Type exit to exit.

--- webmines.py
+++ webmines.py
@@ -1,6 +1,8 @@
 
 import cherrypy
 import os
+import threading
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 class Root:
@@ -12,8 +14,17 @@ class Root:
     def index(self):
         return open('static/index.html').read()
         
+class InputThread(threading.Thread):
+    def run(self):
+        go = True
+        while go:
+            cmd = raw_input(">")
+            if cmd=="exit":
+                go = False
+                cherrypy.engine.exit()
+
+InputThread().start()
 
 if __name__ == "__main__":
     cherrypy.config.update('config.cfg')
-    
-    cherrypy.quickstart(Root())
+    cherrypy.quickstart(Root(),'/')
